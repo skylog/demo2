a880d1ad51b74675bbc5652c5ba5f475537ec69c
Nik <nikbrons@gmail.com>
2024-12-18 18:13:40 +0200

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!1");
+Console.WriteLine("1");
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// Приветствие пользователя
+Console.WriteLine("Добро пожаловать в консольное приложение!");
+
+// Основная логика программы
+Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Привет, {name}!");
+
+// Ожидание завершения программы
+Console.WriteLine("Нажмите любую клавишу для выхода...");
+Console.ReadKey();
